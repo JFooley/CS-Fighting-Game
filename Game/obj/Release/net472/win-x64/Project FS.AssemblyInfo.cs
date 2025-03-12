8917774e5c8f2ad49246f7c2aa7b2eeee21e0e5f
João Gabriel <gabriel.lima.almeida@gmail.com>
2025-03-12 04:17:22 -0300

    a

--- Game/obj/Release/net472/win-x64/Project FS.AssemblyInfo.cs
+++ Game/obj/Release/net472/win-x64/Project FS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61326408d9f70f4d38c87419afbfce0fe181da6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ebb5af90e73ec001ec75e3a65f17d0cb579ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
